c2c6aed2313340febaeb981da4627d5994b1d9db
Jai Mehrotra <94130223+JaiMehrotra02@users.noreply.github.com>
2024-01-09 23:37:57 +0530

    Update api.js

--- dns-manager/src/components/services/api.js
+++ dns-manager/src/components/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dns-xdrc.onrender.com/', // URL of the backend server
+  baseURL: 'dnsbackend-production.up.railway.app/', // URL of the backend server
 });
 
 export default api;
